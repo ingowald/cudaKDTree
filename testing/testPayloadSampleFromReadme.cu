15bfa67fc6bcf6cdd0ce1f0d7b3f3baf7de46673
Ingo Wald <ingowald@gmail.com>
2024-02-05 19:40:47 -0700

    added fcp call to payload sample

--- testing/testPayloadSampleFromReadme.cu
+++ testing/testPayloadSampleFromReadme.cu
@@ -16,6 +16,7 @@
 
 #include "cukd/builder.h"
 #include <random>
+#include "cukd/fcp.h"
 
 #define AS_STRING(x) #x
 #define TO_STRING(x) AS_STRING(x)
@@ -39,13 +40,37 @@ namespace example1 {
     static inline __device__ __host__
     float  get_coord(const PointPlusPayload &data, int dim)
     { return cukd::get_coord(get_point(data),dim); }
+
+    enum { has_explicit_dim = false };
+
+    /*! !{ just defining this for completeness, get/set_dim should never
+      get called for this type because we have set has_explicit_dim
+      set to false. note traversal should ONLY ever call this
+      function for data_t's that define has_explicit_dim to true */
+    static inline __device__ int  get_dim(const PointPlusPayload &) { return -1; }
   };
 
-  void foo(PointPlusPayload *data, int numData) {
+  int divRoundUp(int a, int b) { return (a+b-1)/b; }
+  
+  __global__
+  void callFCP(PointPlusPayload *data, int numData,
+               cukd::box_t<float3> *d_worldBounds)
+  {
+    int tid = threadIdx.x+blockIdx.x*blockIdx.x;
+    if (tid >= numData) return;
+
+    int result = cukd::stackBased::fcp<PointPlusPayload,PointPlusPayload_traits>
+      (data[tid].position,*d_worldBounds,data,numData);
+  }
+  
+  void foo(PointPlusPayload *data, int numData, cukd::box_t<float3> *d_worldBounds)
+  {
     cukd::buildTree
       </* type of the data: */PointPlusPayload,
                               /* traits for this data: */PointPlusPayload_traits>
-      (data,numData);
+      (data,numData,d_worldBounds);
+    
+    callFCP<<<divRoundUp(numData,128),128>>>(data,numData,d_worldBounds);
   }
 
   void test()
@@ -72,7 +97,7 @@ namespace example1 {
     
     // cukd::BUILDER_TO_TEST<PointPlusPayload,PointPlusPayload_traits>
     //   (data,numPointPlusPayloads,worldBounds);
-    foo(data,numPointPlusPayloads);
+    foo(data,numPointPlusPayloads,worldBounds);
 
     std::cout << "world bounds is " << *worldBounds << std::endl;
     CUKD_CUDA_CALL(Free(data));
