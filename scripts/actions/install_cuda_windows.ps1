91ed37f432f63a85f01f6afaad76612e3e9b3f9b
Ingo Wald <ingowald@gmail.com>
2024-09-23 13:50:58 -0600

    fixed CI scripts for ubuntu 24.04

--- scripts/actions/install_cuda_windows.ps1
+++ scripts/actions/install_cuda_windows.ps1
@@ -41,7 +41,7 @@ $CUDA_KNOWN_URLS = @{
     "11.7.1" = "https://developer.download.nvidia.com/compute/cuda/11.7.1/network_installers/cuda_11.7.1_windows_network.exe";
     "11.8.0" = "https://developer.download.nvidia.com/compute/cuda/11.8.0/network_installers/cuda_11.8.0_windows_network.exe";
     "12.0.0" = "https://developer.download.nvidia.com/compute/cuda/12.0.0/network_installers/cuda_12.0.0_windows_network.exe"
-    "12.4.0" = "https://developer.download.nvidia.com/compute/cuda/12.4.0/network_installers/cuda_12.4.0_windows_network.exe"
+    "12.4.0" = "https://developer.download.nvidia.com/compute/cuda/12.4.0/network_installers/cuda_12.4.0_windows_network.exe" 
 }
 
 # @todo - change this to be based on _MSC_VER intead, or invert it to be CUDA keyed instead
